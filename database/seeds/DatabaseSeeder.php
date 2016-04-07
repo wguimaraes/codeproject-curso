5c8c43a9649afe5de6371cfe3f5f256e05141ec5
William Guimaraes <willian.fguim@gmail.com>
2016-04-06 22:48:45 -0300

    Adicionado seeder OAuthClientSeeder para gerar os dados de autenticação para o cliente.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,5 +16,6 @@ class DatabaseSeeder extends Seeder
          $this->call(ProjectTableSeeder::class);
          $this->call(ProjectNoteTableSeeder::class);
          $this->call(ProjectTaskTableSeeder::class);
+         $this->call(OAuthClientSeeder::class);
     }
 }
