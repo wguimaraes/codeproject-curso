79255ebc1f08c8fdc2453586de2f4aa7788416aa
William <willian.fguim@gmail.com>
2016-08-30 09:34:27 -0700

    Iniciando com eventos no angular.

--- resources/assets/js/directives/ProjectFilesDownload.js
+++ resources/assets/js/directives/ProjectFilesDownload.js
@@ -1,31 +1,33 @@
 angular.module('app.directives')
-			.directive('projectFilesDownload', ['appConfig', 'ProjectFiles', function(appConfig, ProjectFiles){
+			.directive('projectFilesDownload', ['$timeout', 'appConfig', 'ProjectFiles', function($timeout, appConfig, ProjectFiles){
 				return {
 					restrict: 'E',
 					templateUrl: appConfig.baseUrl + '/build/views/templates/ProjectFilesDownload.html',
 					link: function(scope, element, attr){
-						
+						var anchor = element.children()[0];
+						scope.$on('salvar-arquivo', function(event, data){
+							$(anchor).removeClass('disabled');
+							$(anchor).text('Download');
+							$(anchor).attr({
+								href: 'data:application-octet-stream;base64,' + data.file,
+								download: data.name
+							});
+							
+							$timeout(function(){
+								scope.downloadFile = function(){
+									
+								};
+								$(anchor)[0].click();
+							});
+						});
 					},
-					controller: ['$scope', '$element', '$attrs', '$timeout', function($scope, $element, $attrs, $timeout){
+					controller: ['$scope', '$element', '$attrs', function($scope, $element, $attrs){
 						$scope.downloadFile = function(){
 							var anchor = $element.children()[0];
 							$(anchor).addClass('disabled');
 							$(anchor).text('Carregando...');
 							ProjectFiles.download({id: null, idFile: $attrs.idFile}, function(data){
-								$(anchor).removeClass('disabled');
-								$(anchor).text('Download');
-								$(anchor).attr({
-									href: 'data:application-octet-stream;base64,' + data.file,
-									download: data.name
-								});
-								
-								$timeout(function(){
-									$scope.downloadFile = function(){
-										
-									};
-									$(anchor)[0].click();
-								});
-								
+								$scope.$emit('salvar-arquivo', data);
 							});
 						}
 					}]
